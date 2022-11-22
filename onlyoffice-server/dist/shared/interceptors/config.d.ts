7873f5eb369138f890fd1c0599e4701ec339ce5f
qinqiaoling <1548062388@qq.com>
2022-11-22 17:39:10 +0800

    Add files via upload

---
+++ onlyoffice-server/dist/shared/interceptors/config.d.ts
@@ -0,0 +1,15 @@
+declare const _default: () => {
+    isTest: boolean;
+    isOnline: boolean;
+    port: string;
+    apiPrefix: string;
+    domain: string;
+    staticPath: string;
+    onlyoffice: {
+        secret: string;
+        domain: string;
+        commandUrl: string;
+        callback: string;
+    };
+};
+export default _default;
